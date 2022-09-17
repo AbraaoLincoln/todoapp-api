c790e80136b95a6e61045bca74b91e762fef3b1c
Abraao Lincol <abraao.lincol@gmail.com>
2022-09-17 12:44:14 -0300

    updating gitignore

--- database/migrations.go
+++ database/migrations.go
@@ -1,13 +1,19 @@
 package database
 
 import (
+	"database/sql"
+
 	"github.com/abraaolincoln/todoapp-api/database/migrations"
 	log "github.com/abraaolincoln/todoapp-api/logger"
 )
 
 func Migrate() {
+	log.Info("Migrating database")
 	db := GetDatabase()
+	migrateTables(db)
+}
 
+func migrateTables(db *sql.DB) {
 	for _, table := range migrations.GetTables() {
 		_, err := db.Exec(table)
 
